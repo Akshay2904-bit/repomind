c8e3f26cab836b562cec4b5abc115d8b22c439f8
Akshay2904-bit <akshaygawde2904@gmail.com>
2026-03-12 01:37:33 +0530

    minor fix: update go.mod and go.sum

---
+++ internal/repository/chunk_code.go
@@ -0,0 +1,9 @@
+package repository
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+    pgvector "github.com/pgvector/pgvector-go"
+	"github.com/Akshay2904-bit/repomind/internal/model"
+)
+
